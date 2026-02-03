c68c9a26642b99c6b58d535d9ba52756021f3347
Abdarhman mohamed <abdarhman1337@gmail.com>
2026-02-03 23:12:40 +0200

    Add files via upload

---
+++ read-write.s
@@ -0,0 +1,27 @@
+.intel_syntax noprefix
+.global _start
+
+_start:
+
+	# read 15 bytes
+mov rax, 0
+mov rdi, 0
+mov rsi, rsp 
+mov rdx, 15
+syscall
+
+
+
+	# write the same 15 bytes on screen
+mov rax, 1
+mov rdi, 1
+mov rsi, rsp
+mov rdx, 15
+syscall
+
+
+
+	# exit the programs
+mov rax, 60
+mov rdi, 0
+syscall
